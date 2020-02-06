603ce43c30bf0f3c675821750dbb6d26e7038688
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-02-06 17:15:02 -0300

    Registra tabela de log no admin

--- sme_portal_aluno_apps/alunos/admin.py
+++ sme_portal_aluno_apps/alunos/admin.py
@@ -1,6 +1,9 @@
+from django.contrib.postgres import fields
+from django_json_widget.widgets import JSONEditorWidget
+
 from django.contrib import admin
 
-from .models import (Aluno, Responsavel)
+from .models import (Aluno, Responsavel, LogConsultaEOL)
 
 
 class AlunoInLine(admin.StackedInline):
@@ -24,3 +27,13 @@ class ResponsavelAdmin(admin.ModelAdmin):
     search_fields = ('uuid', 'cpf', 'nome')
     list_filter = ('status',)
     inlines = [AlunoInLine]
+
+
+@admin.register(LogConsultaEOL)
+class LogConsultaEOLAdmin(admin.ModelAdmin):
+    list_display = ('codigo_eol', 'criado_em',)
+    formfield_overrides = {
+        fields.JSONField: {'widget': JSONEditorWidget},
+    }
+    # fields = ('codigo_eol', 'criado_em', 'json')
+
--- sme_portal_aluno_apps/alunos/models/__init__.py
+++ sme_portal_aluno_apps/alunos/models/__init__.py
@@ -1,2 +1,4 @@
 from .aluno import Aluno
 from .responsavel import Responsavel
+from .log_consulta_eol import LogConsultaEOL
+
