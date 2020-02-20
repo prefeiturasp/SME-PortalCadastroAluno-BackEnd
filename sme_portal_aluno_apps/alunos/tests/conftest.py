fbca38840f348f0bc693c431042fb6502aeec293
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-02-20 11:50:23 -0300

    Atualiza testes

--- sme_portal_aluno_apps/alunos/tests/conftest.py
+++ sme_portal_aluno_apps/alunos/tests/conftest.py
@@ -38,12 +38,13 @@ def payload():
         "codigo_eol": "6541906",
         "data_nascimento": "2014-05-12",
         "responsavel": {
+            "codigo_eol_aluno": "6541906",
             "nm_responsavel": "João das Neves",
             "cd_cpf_responsavel": "12481973221",
             "cd_ddd_celular_responsavel": "27",
             "nr_celular_responsavel": "998391001",
             "email_responsavel": "teste@gmail.com",
-            "dc_tipo_responsavel": 2,
+            "tp_pessoa_responsavel": 2,
             "nome_mae": "Maria das Neves",
             "data_nascimento": "1992-02-08"
         }
