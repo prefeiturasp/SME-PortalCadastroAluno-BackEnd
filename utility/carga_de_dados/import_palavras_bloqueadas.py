776a23b6ba400e9cf4c7cd4eaf2a1f3b7662ddb7
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-02-22 13:46:12 -0300

    Ajusta script para não permitir duplicidade de palavras no importe

--- utility/carga_de_dados/import_palavras_bloqueadas.py
+++ utility/carga_de_dados/import_palavras_bloqueadas.py
@@ -1,11 +1,13 @@
 import csv
 import environ
 from sme_portal_aluno_apps.core.models import ListaPalavrasBloqueadas
+
 ROOT_DIR = environ.Path(__file__) - 1
 
 
 def cria_palavra_bloqueada(palavra):
-    return ListaPalavrasBloqueadas.objects.create(palavra=palavra)
+    palavra_bloqueada, created = ListaPalavrasBloqueadas.objects.update_or_create(palavra=palavra)
+    return palavra_bloqueada
 
 
 class PalavraBloqueada(object):
@@ -15,4 +17,5 @@ class PalavraBloqueada(object):
         with open(f'{ROOT_DIR}/lista_palavras_bloqueadas.csv', 'r') as file:
             lista_palavras = csv.reader(file, delimiter="'")
             for palavra in lista_palavras:
-                cria_palavra_bloqueada(palavra[0])
+                palavra = palavra[0]
+                cria_palavra_bloqueada(palavra)
