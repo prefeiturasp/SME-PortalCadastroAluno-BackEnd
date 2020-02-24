2f10e99b9e897b91eb1ddf9ee601d5c0ce7f645b
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-02-24 17:33:07 -0300

    Finaliza endpoint de listagem de aluno e status
    Com base nos parametros de codigo_eol, nome_estudante e nome_responsavel

--- sme_portal_aluno_apps/alunos/api/viewsets/alunos_viewset.py
+++ sme_portal_aluno_apps/alunos/api/viewsets/alunos_viewset.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from ..serializers.aluno_serializer import (AlunoSerializer, AlunoLookUpSerializer, AlunoCreateSerializer)
-
+from ....eol_servico.utils import EOLService
 from ...models.aluno import Aluno
 
 
@@ -17,17 +17,23 @@ class AlunosViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = self.queryset
         user = self.request.user
+        codigo_eol = self.request.query_params.get('codigo_eol', None)
         nome_estudante = self.request.query_params.get('nome_estudante', None)
         nome_responsavel = self.request.query_params.get('nome_responsavel', None)
+
+        if codigo_eol:
+            queryset = queryset.filter(codigo_eol=codigo_eol)
+            if not queryset and user.codigo_escola:
+                EOLService.cria_aluno_desatualizado(codigo_eol=codigo_eol)
 
         if user.codigo_escola:
             queryset = queryset.filter(codigo_escola=user.codigo_escola)
 
         if nome_estudante:
-            queryset = queryset.filter(nome__contains=nome_estudante)
+            queryset = queryset.filter(nome=nome_estudante)
 
         if nome_responsavel:
-            queryset = queryset.filter(responsavel__nome__contains=nome_responsavel)
+            queryset = queryset.filter(responsavel__nome=nome_responsavel)
 
         return queryset.order_by('nome')
 
