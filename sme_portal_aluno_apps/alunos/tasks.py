dca1f05e0208359628e4c6938f4b49d77c60f086
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-03-06 17:13:30 -0300

    Adiciona log de envio de e-mail na task do celery

--- sme_portal_aluno_apps/alunos/tasks.py
+++ sme_portal_aluno_apps/alunos/tasks.py
@@ -1,3 +1,4 @@
+import logging
 from smtplib import SMTPServerDisconnected
 
 import environ
@@ -6,6 +7,7 @@ from celery import shared_task
 from ..core.helpers.enviar_email import enviar_email_html
 
 env = environ.Env()
+log = logging.getLogger(__name__)
 
 
 # https://docs.celeryproject.org/en/latest/userguide/tasks.html
@@ -15,6 +17,7 @@ env = environ.Env()
     retry_kwargs={'max_retries': 8},
 )
 def enviar_email_solicitacao_uniforme(assunto, template, email, contexto):
+    log.info(f'Enviando email solicitação para: {email}.')
     return enviar_email_html(
         assunto=assunto,
         template=template,
