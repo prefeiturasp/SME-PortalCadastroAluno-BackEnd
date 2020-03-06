f837cc6174b3e52fd40b927416ff7268baa16fd8
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-03-06 17:57:58 -0300

    Adiciona campo codigo eol aluno no list display

--- sme_portal_aluno_apps/alunos/admin.py
+++ sme_portal_aluno_apps/alunos/admin.py
@@ -68,7 +68,7 @@ class ResponsavelAdmin(admin.ModelAdmin):
 
     enviar_emails.short_description = 'Enviar email para responsaveis'
 
-    list_display = ('nome', 'cpf', 'data_nascimento', 'vinculo', 'nome_mae', 'celular', 'email', 'status')
+    list_display = ('nome', 'cpf', 'codigo_eol_aluno', 'data_nascimento', 'vinculo', 'nome_mae', 'celular', 'email', 'status')
     ordering = ('-alterado_em',)
     search_fields = ('uuid', 'cpf', 'nome')
     list_filter = ('status',)
