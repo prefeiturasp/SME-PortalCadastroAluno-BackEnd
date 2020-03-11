e2a3e40b2b76345eb8e7bebd0d4f74dc0bf879ba
anderson89marques <andersonoanjo18@gmail.com>
2020-03-11 16:49:24 -0300

    Salvar na base registro de email

--- sme_portal_aluno_apps/core/admin.py
+++ sme_portal_aluno_apps/core/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import ListaPalavrasBloqueadas
+from .models import ListaPalavrasBloqueadas, Email
 
 
 @admin.register(ListaPalavrasBloqueadas)
 class ListaPalavrasBloqueadasAdmin(admin.ModelAdmin):
     list_display = ('palavra', 'criado_em',)
     search_fields = ('palavra',)
+
+
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('enviar_para', 'criado_em', 'assunto', 'enviado')
+    search_fields = ('enviar_para', 'criado_em', 'enviado')
+
+
--- sme_portal_aluno_apps/core/helpers/enviar_email.py
+++ sme_portal_aluno_apps/core/helpers/enviar_email.py
@@ -5,6 +5,8 @@ from des.models import DynamicEmailConfiguration
 from django.core.mail import send_mail, EmailMessage
 from django.template.loader import render_to_string
 
+from sme_portal_aluno_apps.core.models import Email
+
 logger = logging.getLogger(__name__)
 
 env = environ.Env()
@@ -35,8 +37,10 @@ def enviar_email_html(assunto, template, contexto, enviar_para):
             bcc=(enviar_para,),
         )
         msg.content_subtype = "html"  # Main content is now text/html
-
+        email_sme = Email.objects.create(enviar_para=enviar_para, assunto=assunto, body=msg_html)
         msg.send()
+        email_sme.enviado = True
+        email_sme.save()
 
     except Exception as err:
         logger.error(str(err))
