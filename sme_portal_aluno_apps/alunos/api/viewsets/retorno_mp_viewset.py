02128d45c0f65ca722e672aecf388bef2d76cb81
Kelwy <kelwyduarte@gmail.com>
2020-09-01 15:58:55 -0300

    Remove status de cpf bloqueado e ajusta viewset

--- sme_portal_aluno_apps/alunos/api/viewsets/retorno_mp_viewset.py
+++ sme_portal_aluno_apps/alunos/api/viewsets/retorno_mp_viewset.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, mixins
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from ...models import RetornoMP
 from ..serializers.retorno_mp_serializer import (RetornoMPSerializer, RetornoMPCreateSerializer)
 
@@ -11,18 +11,7 @@ class RetornoMPViewset(mixins.CreateModelMixin,
     queryset = RetornoMP.objects.all()
     serializer_class = RetornoMPSerializer
 
-    def create(self, request, *args, **kwargs):
-        for retorno in request.data.get('retornos'):
-            novo_retorno = RetornoMP(
-                codigo_eol=retorno.get('codigo_eol'),
-                cpf=retorno.get('cpf'),
-                status=retorno.get('status'),
-                mensagem=retorno.get('mensagem'),
-                data_ocorrencia=retorno.get('data_ocorrencia'),
-            )
-            novo_retorno.save()
-
-    # def get_serializer_class(self):
-    #     if self.action == 'create':
-    #         return RetornoMPCreateSerializer
-    #     return RetornoMPSerializer
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return RetornoMPCreateSerializer
+        return RetornoMPSerializer
